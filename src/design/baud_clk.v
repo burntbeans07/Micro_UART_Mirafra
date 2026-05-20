bc9a81c0b4493950a1f77bc28d2ff9a367cc19c8
Srinidhi Balaji <132289138+burntbeans07@users.noreply.github.com>
2026-05-20 09:54:27 +0530

    Create baud_clk.v

---
+++ src/design/baud_clk.v
@@ -0,0 +1,22 @@
+`default_nettype none
+module baud_clk1#(parameter BAUD=2400)(
+    input wire clk,rst,
+    output reg clk_baud
+   );
+   
+   localparam VAL= 100_000_000/(BAUD*16*2);
+   reg [31:0]count;
+   always @ (posedge clk or negedge rst) begin
+    if(!rst)begin
+        count<=0;
+        clk_baud<=0;
+    end
+    else if (count==VAL-1)begin
+        count<=0;
+        clk_baud<=~clk_baud;
+    end
+    else
+        count<=count+1;
+   end
+     
+endmodule
